eee5abf62575f5ac0329ab61e7ee92df20f9dd87
hossidev <hossi@hossi.is>
2025-03-14 16:31:52 +0000

    Enhance ticket deletion process and improve link prefetching: - Added revalidation for the ticket list after a ticket is deleted to ensure up-to-date data. - Updated the ticket link in TicketItem component to use prefetching for improved performance.

--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { prisma } from "@/lib/prisma";
 import { ticketsPath } from "@/paths";
@@ -11,6 +12,8 @@ export const deleteTicket = async (ticketId: string) => {
       id: ticketId,
     },
   });
+
+  revalidatePath(ticketsPath());
 
   redirect(ticketsPath());
 };
