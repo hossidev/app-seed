1d035d0cc01c8f55a908618707ddf150f60ddd81
hossidev <hossi@hossi.is>
2025-03-15 14:47:50 +0000

    Enhance ticket upsert functionality and add validation:
    - Introduced Zod schema for validating ticket data in the upsertTicket function.
    - Updated the TicketUpsertForm component to utilize action state for better user feedback.
    - Added the 'zod' dependency to package.json and package-lock.json for schema validation support.

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -2,26 +2,45 @@
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { z } from "zod";
+import {
+  ActionState,
+  fromErrorToActionState,
+} from "@/components/form/utils/to-action-state";
 import { prisma } from "@/lib/prisma";
 import { ticketPath, ticketsPath } from "@/paths";
 
-export const upsertTicket = async (id: string | "", formData: FormData) => {
-  const data = {
-    title: formData.get("title") as string,
-    content: formData.get("content") as string,
-  };
+const upsertTicketSchema = z.object({
+  title: z.string().min(1).max(191),
+  content: z.string().min(1).max(1024),
+});
 
-  await prisma.ticket.upsert({
-    where: {
-      id: id || "",
-    },
-    update: data,
-    create: data,
-  });
+export const upsertTicket = async (
+  id: string | "",
+  _actionState: ActionState,
+  formData: FormData
+) => {
+  try {
+    const data = upsertTicketSchema.parse({
+      title: formData.get("title"),
+      content: formData.get("content"),
+    });
+
+    await prisma.ticket.upsert({
+      where: {
+        id: id || "",
+      },
+      update: data,
+      create: data,
+    });
+  } catch (error) {
+    return fromErrorToActionState(error, formData);
+  }
 
   revalidatePath(ticketsPath());
 
   if (id) {
     redirect(ticketPath(id));
   }
+  return { message: "Ticket created successfully" };
 };
